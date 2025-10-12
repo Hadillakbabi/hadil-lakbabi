1db89b68c9d3036df541f32de8ac216d98b97374
Hadillakbabi <hadillakbabi@gmail.com>
2025-10-12 14:45:18 +0200

    Add files via upload

--- TD/Exercice2/main.c
+++ TD/Exercice2/main.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
+#include <time.h>
+#include "prime.h"
 
+int main() {
+    int n;
+    printf("Entrez un nombre n : ");
+    scanf("%d", &n);
+
+    clock_t start, end;
+    double time_naive, time_efficient;
+
+    // Mesure de la version naïve
+    start = clock();
+    int count_naive = prime_numbers_naive(n);
+    end = clock();
+    time_naive = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+    // Mesure de la version efficace
+    start = clock();
+    int count_efficient = prime_numbers_efficient(n);
+    end = clock();
+    time_efficient = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+    printf("\nRésultats :\n");
+    printf("Naive      : %d nombres premiers trouvés en %.6f secondes\n", count_naive, time_naive);
+    printf("Efficient  : %d nombres premiers trouvés en %.6f secondes\n", count_efficient, time_efficient);
+
+    if (time_naive < time_efficient)
+        printf("\n La méthode naïve est plus rapide (étrange, mais possible pour petits n)\n");
+    else if (time_naive > time_efficient)
+        printf("\n La méthode efficace (crible) est plus rapide\n");
+    else
+        printf("\n Les deux méthodes ont le même temps d'exécution\n");
+
+    return 0;
+}
